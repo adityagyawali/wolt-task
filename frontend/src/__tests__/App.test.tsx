c5ace9d8c3e10935f942266e2cf92444e3164268
adityagyawali <aditya@rensoriginal.com>
2021-01-28 21:22:56 +0200

    Fix typo Add test script in the front end

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from '../App'
 
-test('renders learn react link', () => {
+test('renders Helsinki button element', () => {
   render(<App />)
-  const linkElement = screen.getByText(/Login/i)
+  const linkElement = screen.getByText(/Helsinki/i)
   expect(linkElement).toBeInTheDocument()
 })
